d5a506a1b1a7011dee757b368bcb56f183923a40
Saulo Mendes <saulodom13@gmail.com>
2018-10-29 07:49:29 -0300

    Upload BackEnd

---
+++ BackEnd/src/JsonFormatter.java
@@ -0,0 +1,10 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public interface JsonFormatter {
+	public JSONObject toJson();
+	
+	public default JSONArray toJsonArray() {
+		return new JSONArray().put(toJson());
+	}
+}
